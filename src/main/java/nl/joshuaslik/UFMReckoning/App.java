c1874a3bfee87d06cae6462b6971ac3b9613c843
Joshua Slik <joshua.b.slik@gmail.com>
2014-11-18 18:32:26 +0100

    Started work on XML Parser

--- src/main/java/nl/joshuaslik/UFMReckoning/App.java
+++ src/main/java/nl/joshuaslik/UFMReckoning/App.java
@@ -1,11 +1,57 @@
 package nl.joshuaslik.UFMReckoning;
 
-public class App 
-{
-    public static void main(String[] args)
-    {
-    	System.out.println("Hello World!");
-    	System.out.println("One day this will be an awesome Football Manager!");
-        // TODO Insert main class logic here. This is the class to be executed.
-    }
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.security.CodeSource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class App {
+	
+	public static void main(String[] args) throws IOException {
+		SAXParser.parse("players/adamsarota.xml");
+		String[] files = filesInAlt("players");
+		for(int i = 0; i < files.length; i++) 
+			SAXParser.parse(files[i]);
+		for(int i = 0; i < files.length; i++) 
+			System.out.println(files[i]);
+		System.out.println(files.length);
+	}
+	
+	public static File[] filesIn(String folderpath) {
+		File folder = new File(folderpath);
+		File[] listOfFiles = folder.listFiles();
+		
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				System.out.println("File " + listOfFiles[i].getName());
+		    }
+		}
+		
+		return listOfFiles;
+	}
+	
+	public static String[] filesInAlt(String folderpath) throws IOException {
+		CodeSource src = App.class.getProtectionDomain().getCodeSource();
+		List<String> list = new ArrayList<String>();
+
+		if( src != null ) {
+		    URL jar = src.getLocation();
+		    ZipInputStream zip = new ZipInputStream( jar.openStream());
+		    ZipEntry ze = null;
+
+		    while( ( ze = zip.getNextEntry() ) != null ) {
+		        String entryName = ze.getName();
+		        if( entryName.startsWith("players") && entryName.endsWith(".xml") ) {
+		            list.add( entryName  );
+		        }
+		    }
+
+		 }
+		 return list.toArray( new String[ list.size() ] );
+	}
+	
 }
---
+++ src/main/java/nl/joshuaslik/UFMReckoning/SAXParser.java
@@ -0,0 +1,118 @@
+package nl.joshuaslik.UFMReckoning;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.helpers.XMLReaderFactory;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * @author Joshua Slik<br>
+ *         http://www.joshuaslik.nl/
+ *
+ */
+public class SAXParser extends DefaultHandler {
+
+	public static void main(String args[]) throws Exception {
+		XMLReader xr = XMLReaderFactory.createXMLReader();
+		SAXParser handler = new SAXParser();
+		xr.setContentHandler(handler);
+		xr.setErrorHandler(handler);
+
+		// Parse each file provided on the command line.
+		for (int i = 0; i < args.length; i++) {
+			FileReader r = new FileReader(args[i]);
+			xr.parse(new InputSource(r));
+		}
+	}
+
+	public static void parse(String filename) {
+		XMLReader xr = null;
+		try {
+			xr = XMLReaderFactory.createXMLReader();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
+		SAXParser handler = new SAXParser();
+		xr.setContentHandler(handler);
+		xr.setErrorHandler(handler);
+
+		// Parse each file provided on the command line.
+		ClassLoader classLoader = App.class.getClassLoader();
+		InputStream is = classLoader.getResourceAsStream(filename);
+		try {
+			xr.parse(new InputSource(is));
+		} catch (IOException | SAXException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public SAXParser() {
+		super();
+	}
+
+	// //////////////////////////////////////////////////////////////////
+	// Event handlers.
+	// //////////////////////////////////////////////////////////////////
+
+	public void startDocument() {
+		System.out.println("Start document");
+	}
+
+	public void endDocument() {
+		System.out.println("End document");
+	}
+
+	public void startElement(String uri, String name, String qName,
+			Attributes atts) {
+		if ("".equals(uri)) {
+			System.out.println("Start element: " + qName);
+			for (int i = 0; i < atts.getLength(); i++) {
+				System.out.println("   Attribute : " + atts.getQName(i) + "=\""
+						+ atts.getValue(i) + "\"");
+			}
+		} else {
+			System.out.println("Start element: {" + uri + "}" + name);
+		}
+	}
+
+	public void endElement(String uri, String name, String qName) {
+		if ("".equals(uri))
+			System.out.println("End element: " + qName);
+		else
+			System.out.println("End element:   {" + uri + "}" + name);
+	}
+
+	public void characters(char ch[], int start, int length) {
+		System.out.print("   Characters: \"");
+		for (int i = start; i < start + length; i++) {
+			switch (ch[i]) {
+			case '\\':
+				System.out.print("\\\\");
+				break;
+			case '"':
+				System.out.print("\\\"");
+				break;
+			case '\n':
+				System.out.print("\\n");
+				break;
+			case '\r':
+				System.out.print("\\r");
+				break;
+			case '\t':
+				System.out.print("\\t");
+				break;
+			default:
+				System.out.print(ch[i]);
+				break;
+			}
+		}
+		System.out.print("\"\n");
+	}
+
+}
