6e0df9db9eff22a323b783c4fdc093d170ff13e8
Joshua Slik <joshua.b.slik@gmail.com>
2014-11-19 11:53:00 +0100

    Javadoc configuration in pom.xml and added package info files

---
+++ src/main/java/nl/joshuaslik/UFMReckoning/XMLToolkit/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Toolkit containing XML parsing, reading and saving tools
+ * @author <a href="http://www.joshuaslik.nl/" target="_blank">Joshua Slik</a>
+ *
+ */
+package nl.joshuaslik.UFMReckoning.XMLToolkit;
---
+++ src/main/java/nl/joshuaslik/UFMReckoning/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Unreal Football Manager: Reckoning<br>
+ * a game designed by TU Delft Students
+ * 
+ * @author Sander Benoist
+ * @author Robert Haaring
+ * @author Naomi de Ridder
+ * @author <a href="http://www.joshuaslik.nl/" target="_blank">Joshua Slik</a>
+ * @author Lisette Veltkamp
+ * @author Bryan van Wijk
+ *
+ */
+package nl.joshuaslik.UFMReckoning;
