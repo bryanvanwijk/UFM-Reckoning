fa79d6b87c203eb18bad2fb7c000a474e8f10b7c
Joshua Slik <joshua.b.slik@gmail.com>
2014-12-03 12:17:49 +0100

    Added XML Save functionality and expanded documentation for XML classes

--- src/main/java/nl/joshuaslik/UFMReckoning/XMLToolkit/XMLFile.java
+++ src/main/java/nl/joshuaslik/UFMReckoning/XMLToolkit/XMLFile.java
@@ -1,5 +1,12 @@
 package nl.joshuaslik.UFMReckoning.XMLToolkit;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+
 /**
  *
  * @author <a href="http://www.joshuaslik.nl/" target="_blank">Joshua Slik</a>
@@ -11,6 +18,9 @@ public class XMLFile {
 
 	/**
 	 * Constructor
+	 * 
+	 * @param root
+	 * 				root tag of this file
 	 */
 	public XMLFile(XMLTag root) {
 		this.root = root;
@@ -19,18 +29,74 @@ public class XMLFile {
 	/**
 	 * 
 	 * @param name
-	 *            is the name of the element to get
+	 * 				is the name of the element to get
+	 * @return
+	 * 				the content of the element at location <b>name</b>
 	 * @throws NoSuchElementException
-	 *             Is thrown if element is not found
+	 *				is thrown if element is not found
 	 */
 	public String getContent(String name) throws NoSuchElementException {
 		return root.getContent(name);
 	}
 	
+	/**
+	 * 
+	 * @param name
+	 * 				is the name of the element to get
+	 * @return
+	 * 				the element at the location <b>name</b>
+	 * @throws NoSuchElementException
+	 * 				is thrown if element is not found
+	 */
 	public XMLTag getElement(String name) throws NoSuchElementException {
 		return root.getElement(name);
 	}
-
+	
+	/**
+	 * This method is depreciated. Please use {@link XMLTag#getAttribute(String)} until functionality has been added.
+	 * 
+	 * This method will get the attribute of an element in this XMLFile.
+	 * It expects that you specify where to find the attribute like so:
+	 * roottag.child1.child2.targetelement.attributeX
+	 * @param name
+	 * 				is the name of the attribute to get
+	 * @return
+	 * 				the attribute from element <b>name</b>
+	 * @deprecated
+	 */
+	public String getAttribute(String name) {
+		String retstr = null;
+		return retstr;
+	}
+	
+	/**
+	 * 
+	 * @param location
+	 * 				the location to save this XMLFile
+	 */
+	public void save(String location) {
+		String retstr = root.toString();
+		String encoding = "UTF-8";
+		File target = new File(location);
+		PrintWriter writer = null;
+		
+		try {
+			writer = new PrintWriter(target, encoding);
+		} catch (FileNotFoundException e) {
+			System.err.println("[ERROR] The file \"" + target + "\" was not found!");
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			System.err.println("[ERROR] The encoding \"" + encoding + "\" is not supported!");
+			e.printStackTrace();
+		}
+		writer.write(retstr);
+		writer.flush();
+	}
+	
+	/**
+	 * @return
+	 * 				a string representation of this object.
+	 */
 	public String toString() {
 		return root.toString();
 	}
