3bbbceb1b75063ef357e218ebe1b1c55c133b452
Joshua Slik <joshua.b.slik@gmail.com>
2014-12-07 14:19:49 +0100

    Created classes for Game and User

---
+++ src/main/java/nl/joshuaslik/UFMReckoning/backend/Game.java
@@ -0,0 +1,10 @@
+package nl.joshuaslik.UFMReckoning.backend;
+
+/**
+ * @author <a href="http://www.joshuaslik.nl/" target="_blank">Joshua Slik</a>
+ * @author Naomi de Ridder
+ *
+ */
+public class Game {
+
+}
---
+++ src/main/java/nl/joshuaslik/UFMReckoning/backend/User.java
@@ -0,0 +1,10 @@
+package nl.joshuaslik.UFMReckoning.backend;
+
+/**
+ * @author Naomi de Ridder
+ * @author <a href="http://www.joshuaslik.nl/" target="_blank">Joshua Slik</a>
+ *
+ */
+public class User {
+
+}
