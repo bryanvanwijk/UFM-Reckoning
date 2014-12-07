32da6b96c6401683f3d25c73358541dec69caaf5
Naomi de Ridder <naomi-ah@hotmail.com>
2014-12-07 16:53:38 +0100

    Tests added for User Class and subclasses.

---
+++ src/test/java/nl/joshuaslik/UFMReckoning/backend/HumanTest.java
@@ -0,0 +1,14 @@
+package nl.joshuaslik.UFMReckoning.backend;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HumanTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/nl/joshuaslik/UFMReckoning/backend/PCTest.java
@@ -0,0 +1,14 @@
+package nl.joshuaslik.UFMReckoning.backend;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PCTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
