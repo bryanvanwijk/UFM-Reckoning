7ad282118a7ee01fdf5966275d6996133dc01907
Joshua Slik <joshua.b.slik@gmail.com>
2014-12-07 15:49:53 +0100

    Started work on Game Class

--- src/main/java/nl/joshuaslik/UFMReckoning/backend/User.java
+++ src/main/java/nl/joshuaslik/UFMReckoning/backend/User.java
@@ -5,16 +5,23 @@ package nl.joshuaslik.UFMReckoning.backend;
  * @author <a href="http://www.joshuaslik.nl/" target="_blank">Joshua Slik</a>
  *
  */
-public class User {
+public abstract class User {
 
 	private Team team;
 	private String userName;
-	private int budget; 
+	private int budget;
 	
 	public User(Team teamIn, String userNameIn, int budgetIn) {
 		team = teamIn;
 		userName = userNameIn; 
 		budget = budgetIn;
+	}
+	
+	public boolean isHuman() {
+		if(this instanceof Human) {
+			return true;
+		}
+		return false;
 	}
 	
 	
