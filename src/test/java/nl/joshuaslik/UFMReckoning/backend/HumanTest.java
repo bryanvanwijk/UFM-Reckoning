5a9668c202642a8fec3b7c864d9413c9f5f01faf
Joshua Slik <joshua.b.slik@gmail.com>
2014-12-07 17:01:42 +0100

    Fixed tests for PC and Human

--- src/test/java/nl/joshuaslik/UFMReckoning/backend/HumanTest.java
+++ src/test/java/nl/joshuaslik/UFMReckoning/backend/HumanTest.java
@@ -1,14 +1,39 @@
 package nl.joshuaslik.UFMReckoning.backend;
 
-import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
 
-public class HumanTest {
+import static org.junit.Assert.*;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+public class HumanTest {
+	
+	User user;
+	
+	@Before
+	public void construct() {
+		user = new PC(new Team("tmname", "cchname"), "username", 1000000);
+	}
+	
+	public void testConstructor() {
+		construct();
 	}
 
+	@Test
+	public void testGetUserName() {
+		construct();
+		assertTrue(user.getUserName().equals("username"));
+	}
+	
+	@Test
+	public void testGetTeam() {
+		construct();
+		assertTrue(user.getTeam().getTeamName().equals("tmname"));
+		assertTrue(user.getTeam().getCoachName().equals("cchname"));
+	}
+	
+	@Test
+	public void testGetBudget() {
+		construct();
+		assertTrue(user.getBudget() == 1000000);
+	}
 }
