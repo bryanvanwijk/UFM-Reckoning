e32da6ef4c21d41c6d971b522682d75608bf914c
Joshua Slik <joshua.b.slik@gmail.com>
2014-12-08 13:10:48 +0100

    Written tests for XMLTag Class

--- src/test/java/nl/joshuaslik/UFMReckoning/XMLToolkit/XMLTagTest.java
+++ src/test/java/nl/joshuaslik/UFMReckoning/XMLToolkit/XMLTagTest.java
@@ -1,5 +1,9 @@
 package nl.joshuaslik.UFMReckoning.XMLToolkit;
 
+import static org.junit.Assert.*;
+
+import java.util.LinkedHashMap;
+
 import org.junit.Test;
 
 /**
@@ -8,8 +12,58 @@ import org.junit.Test;
  */
 public class XMLTagTest {
 
+	XMLTag tag;
+
+	public void construct() {
+		LinkedHashMap<String, String> atts = new LinkedHashMap<String, String>();
+		atts.put("attribute1", "value1");
+		atts.put("attribute2", "value2");
+		tag = new XMLTag("rootname", atts);
+		tag.setContent("rootcontent");
+		atts = new LinkedHashMap<String, String>();
+		atts.put("subattribute1", "subvalue1");
+		atts.put("subattribute2", "subvalue2");
+		XMLTag sub = new XMLTag("subname", atts);
+		sub.setContent("subcontent");
+		tag.addElement(sub);
+	}
+
 	@Test
-	public void test1() {
+	public void testConstructor() {
+		construct();
+	}
+
+	@Test
+	public void testGetName() {
+		construct();
+		assertTrue(tag.getName().equals("rootname"));
+	}
+	
+	@Test
+	public void testGetContent() {
+		construct();
+		assertTrue(tag.getContent().equals("rootcontent"));
+		try {
+			assertTrue(tag.getContent("rootname.subname").equals("subcontent"));
+		} catch (NoSuchElementException e) {
+			// TODO Auto-generated catch block
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void getElement() {
+		construct();
+		XMLTag sub = null;
+		try {
+			sub = tag.getElement("subname");
+		} catch (NoSuchElementException e) {
+			// TODO Auto-generated catch block
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+		assertTrue(sub.getContent().equals("subcontent"));
 	}
 
 }
