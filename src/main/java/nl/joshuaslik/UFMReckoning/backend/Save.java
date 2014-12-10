a2a7705fbf92e4239392c565fe59ceab4318850c
Joshua Slik <joshua.b.slik@gmail.com>
2014-12-10 16:00:38 +0100

    Created Save Class

---
+++ src/main/java/nl/joshuaslik/UFMReckoning/backend/Save.java
@@ -0,0 +1,10 @@
+package nl.joshuaslik.UFMReckoning.backend;
+
+/**
+ * @author Bryan van Wijk
+ * @author <a href="http://www.joshuaslik.nl/" target="_blank">Joshua Slik</a>
+ *
+ */
+public class Save {
+
+}
