47547092815237ffadd646cb6cbe82368b190b34
Lisette V <lisette_stella@hotmail.com>
2014-12-16 20:25:14 +0100

    Added textfield to NewGame

--- src/main/java/nl/joshuaslik/UFMReckoning/gui/NewGame.java
+++ src/main/java/nl/joshuaslik/UFMReckoning/gui/NewGame.java
@@ -6,6 +6,7 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -13,30 +14,42 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class NewGame {
+	static String username;
 
-	public static void start() {		
-
+	/*
+	 * Start
+	 * Text, buttons en textfield + events die bij de scene horen
+	 */
+	public static void start() {
 		Text t = new Text(400, 300, "Enter your name");
-		t.setFont(Font.font("Maiandra", FontWeight.BOLD, 60));
-		//Label lab = new Label("Enter your name");
-		Button but = new Button("Continue");
-
+		t.setFont(Font.font("Maiandra GD", FontWeight.BOLD, 60));
+		Button but = new Button("Submit");
+		
+		TextField txt = new TextField();
+		txt.setPrefWidth(300);
+		
 		but.setOnAction(new EventHandler<ActionEvent>() {
 
 			@Override
 			public void handle(ActionEvent arg0) {
-				//lab.setText("Loading...");
+				t.setText("Loading...");
+				String user = txt.getText();
+				username = user;
 			}
 		});
 
 		VBox root = new VBox();
-		root.getChildren().addAll(t, but);
+		root.getChildren().addAll(t, txt, but);
 
 		Scene scene = new Scene(root, 1920, 1080);
 		Main.setScene(scene);
 	}
+	
+	public String getUsername() {
+		return username;
+	}
 
-	public static void PickUserName() {
-		String Username;
+	public void setUsername(String Username) {
+		this.username = username;
 	}
 }
