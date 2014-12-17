2d3b64bbee346adc5042070a7d5a7897857871c3
Naomi de Ridder <naomi-ah@hotmail.com>
2014-12-17 11:22:50 +0100

    Changed on action for competition class.

--- src/main/java/nl/joshuaslik/UFMReckoning/gui/game/CompetitionController.java
+++ src/main/java/nl/joshuaslik/UFMReckoning/gui/game/CompetitionController.java
@@ -1,5 +1,29 @@
 package nl.joshuaslik.UFMReckoning.gui.game;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class CompetitionController {
+
+	
+	@SuppressWarnings("unused")
+	@FXML
+	protected void handleSeeAllGames(ActionEvent event) throws IOException {
+		SeeAllGames.start();
+	}
+
+	@SuppressWarnings("unused")
+	@FXML
+	protected void handleRanking(ActionEvent event) throws IOException {
+		Ranking.start();
+	}
+
+	@SuppressWarnings("unused")
+	@FXML
+	protected void handleShowNextGame(ActionEvent event) throws IOException {
+		ShowNextGame.start();
+	}
 
 }
